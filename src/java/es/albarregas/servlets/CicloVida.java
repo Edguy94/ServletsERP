f5b3f5a9cd0940f0845b0a36697d9be34e8624e9
DESKTOP-I7B4K59\rpk19 <rpk1994@gmail.com>
2016-10-01 21:40:02 +0200

    Todo funcionando (TODO ESTILOS)

---
+++ src/java/es/albarregas/servlets/CicloVida.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.albarregas.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author rpk19
+ */
+public class CicloVida extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      PrintWriter out = resp.getWriter();
+        out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet HolaMundo</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1> El ciclo de vida se muestra por consola </h1>");
+            out.println("</body>");
+            out.println("</html>");
+        System.out.println("Estoy en el service");
+    }
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("Estoy en el init");
+    }
+
+    @Override
+    public void destroy() {
+      System.out.println("Estoy en el destroy");
+    }
+    
+    
+
+}
